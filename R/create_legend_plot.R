cd907dca42577ec6d647637e153c7c8dbc7cc482
Gertjan <gertjan.verhoeven@gmail.com>
2025-09-21 11:53:14 +0200

    code formatting

--- R/create_legend_plot.R
+++ R/create_legend_plot.R
@@ -5,17 +5,17 @@ create_legend_plot <- function(df, n_legend_items){
 
   # Create a data frame for the legend
   legend_df <- df  %>%
-    ungroup() %>% 
-    select(skill_name, color) %>% 
-    distinct(skill_name, color) 
-  
+    ungroup() %>%
+    select(skill_name, color) %>%
+    distinct(skill_name, color)
+
 
   # Generate a custom legend using ggplot
   legend_plot <- ggplot(legend_df, aes(x = 1, y = skill_name, fill = color)) +
     # The width is proportional to the number of items to display because for some reasons, the more the items, the smaller the width becomes in default settings
     geom_tile(color = "black", width = 2.8*n_legend_items/11) +
     # Add a shadow to make white (added on the following line) readable on light background. The nudge_x offset is there because for some reasons, the text is not aligned left, despite hjust = 0. It is proportional to the number of items because the bigger the width (proportional to the number of items), the bigger the disalignment
-    geom_text(aes(label = skill_name), hjust = 0, color = "black", nudge_x = -1.4*n_legend_items/11-0.02, nudge_y = -0.02) + 
+    geom_text(aes(label = skill_name), hjust = 0, color = "black", nudge_x = -1.4*n_legend_items/11-0.02, nudge_y = -0.02) +
     geom_text(aes(label = skill_name), hjust = 0, color = "white", nudge_x = -1.4*n_legend_items/11) +
     scale_fill_identity() +
     coord_fixed() +
