8978c409cf09b07f48b2844f783859c4c97309d7
Gertjan <gertjan.verhoeven@gmail.com>
2025-09-22 22:07:17 +0200

    more dplyr function imports

--- R/bbrosterplots-package.R
+++ R/bbrosterplots-package.R
@@ -2,11 +2,16 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr case_when
 #' @importFrom dplyr distinct
 #' @importFrom dplyr filter
+#' @importFrom dplyr first
 #' @importFrom dplyr group_by
 #' @importFrom dplyr mutate
+#' @importFrom dplyr n
 #' @importFrom dplyr n_distinct
+#' @importFrom dplyr rename
 #' @importFrom dplyr right_join
 #' @importFrom dplyr row_number
 #' @importFrom dplyr select
