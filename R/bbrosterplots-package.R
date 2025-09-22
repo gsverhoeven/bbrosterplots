4d0be0ed6dfb7473b933fcad8426088d1bb4bf6b
Gertjan <gertjan.verhoeven@gmail.com>
2025-09-22 07:30:10 +0200

    add dplyr dependencies as importFrom

---
+++ R/bbrosterplots-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr n_distinct
+#' @importFrom dplyr right_join
+#' @importFrom dplyr row_number
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+## usethis namespace: end
+NULL
