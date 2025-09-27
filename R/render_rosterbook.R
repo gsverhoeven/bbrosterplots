a46dd168e6d916c2ec790e32c64a1af46c5988b7
Gertjan <gertjan.verhoeven@gmail.com>
2025-09-27 23:42:32 +0200

    replace existing copy of roster book template

--- R/render_rosterbook.R
+++ R/render_rosterbook.R
@@ -4,6 +4,6 @@
 
 #' @export
 render_rosterbook <- function(target_dir = "."){
-  file.copy(from = system.file("extdata", "roster_book_template.Rmd", package = "bbrosterplots"), to = "roster_book.Rmd")
+  file.copy(from = system.file("extdata", "roster_book_template.Rmd", package = "bbrosterplots"), to = "roster_book.Rmd", overwrite = TRUE)
   rmarkdown::render("roster_book.Rmd")
 }
