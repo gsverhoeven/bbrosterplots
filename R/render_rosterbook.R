f920f3434c36f7500da822c4cf088ab554516a37
Gertjan <gertjan.verhoeven@gmail.com>
2025-10-02 22:16:05 +0200

    add params and refresh arguments

--- R/render_rosterbook.R
+++ R/render_rosterbook.R
@@ -3,7 +3,9 @@
 #' @returns Nothing.
 
 #' @export
-render_rosterbook <- function(target_dir = "."){
-  file.copy(from = system.file("extdata", "roster_book_template.Rmd", package = "bbrosterplots"), to = "roster_book.Rmd", overwrite = TRUE)
-  rmarkdown::render("roster_book.Rmd")
+render_rosterbook <- function(target_dir = ".", params, refresh = TRUE){
+  if(refresh){
+    file.copy(from = system.file("extdata", "roster_book_template.Rmd", package = "bbrosterplots"), to = "roster_book.Rmd", overwrite = TRUE)
+  }
+  rmarkdown::render("roster_book.Rmd", params = params)
 }
