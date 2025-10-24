caa7eb7a2f20cf2e55b94027b1326b978e84ea83
Gertjan <gertjan.verhoeven@gmail.com>
2025-10-24 17:35:00 +0200

    add documentation for datasets

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' FUMBBL Super League Season 6 roster data
+#'
+#' @description
+#' Blood Bowl rosters from the 6th season of Super League, a competitive online
+#' tournament where coaches are divided in divisions and can be promoted
+#' or delegated, depending on the results.
+#'
+#' @format ## `df_rosters`
+#' A data frame with 3,573 rows and 8 columns:
+#' \describe{
+#'   \item{number}{number}
+#'   \item{position}{position name}
+#'   \item{team_id}{team_id}
+#' }
+#' @source <https://www.fumbbl.com>
+"df_rosters"
+#' @rdname df_rosters
+
+#' @format ## `df_rosters1`
+#' A data frame with 3,521 rows and 17 columns.
+"df_rosters1"
+#' @rdname df_rosters
